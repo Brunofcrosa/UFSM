8782f8ce791b5ca85de20e13fc68b55f1abae860
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-11-26 13:35:04 -0300

    fix

--- EstruturaDeDados/Atividades/1.c
+++ EstruturaDeDados/Atividades/1.c
@@ -1,32 +1,166 @@
-/** Faça um programa que receba dois valores e, utilizando ponteiros, retorne o maior e
-menor deles. O programa deve ter um controle sobre valores iguais.
-**/
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-void encontraMaiorMenor(int *num1, int *num2, int *maior, int *menor) {
-    if (*num1 > *num2) {
-        *maior = *num1;
-        *menor = *num2;
-    } else if (*num2 > *num1) {
-        *maior = *num2;
-        *menor = *num1;
-    } else {
-        printf("Os valores são iguais.\n");
-        *maior = *num1;
-        *menor = *num1;
+#define N 7
+
+void imprimeGrafo(int grafo[N][N], char paginas[N][20]) {
+    printf("\n%12s", "");
+    for (int k = 0; k < N; k++) {
+        printf(" %-10.10s", paginas[k]);
     }
+    printf("\n");
+
+    for (int i = 0; i < N; i++) {
+        printf("%-12s", paginas[i]);
+        for (int j = 0; j < N; j++) {
+            printf(" %-10d", grafo[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void listarAlcancaveis(int grafo[N][N], char paginas[N][20], int inicio) {
+    if (inicio < 0 || inicio >= N) {
+        printf("Indice invalido.\n");
+        return;
+    }
+
+    bool visitado[N];
+    for(int i = 0; i < N; i++) visitado[i] = false;
+
+    int fila[N];
+    int frente = 0;
+    int tras = 0;
+
+    fila[tras++] = inicio;
+    visitado[inicio] = true;
+
+    printf("Paginas alcancaveis a partir de '%s':\n", paginas[inicio]);
+
+    bool encontrouAlguma = false;
+
+    while (frente < tras) {
+        int atual = fila[frente++];
+
+        for (int i = 0; i < N; i++) {
+            if (grafo[atual][i] == 1 && !visitado[i]) {
+                visitado[i] = true;
+                fila[tras++] = i;
+                printf("- %s\n", paginas[i]);
+                encontrouAlguma = true;
+            }
+        }
+    }
+
+    if (!encontrouAlguma) {
+        printf("(Nenhuma pagina alcancavel)\n");
+    }
+    printf("\n");
+}
+
+void paginasPopulares(int grafo[N][N], char paginas[N][20]) {
+    printf("Paginas Populares (Grau de entrada > 1):\n");
+    
+    for (int j = 0; j < N; j++) {
+        int grauEntrada = 0;
+        for (int i = 0; i < N; i++) {
+            if (grafo[i][j] == 1) {
+                grauEntrada++;
+            }
+        }
+
+        if (grauEntrada > 1) {
+            printf("- %s (Entradas: %d)\n", paginas[j], grauEntrada);
+        }
+    }
+    printf("\n");
+}
+
+void paginasSemSaida(int grafo[N][N], char paginas[N][20]) {
+    printf("Paginas Sem Saida (Grau de saida = 0):\n");
+    
+    for (int i = 0; i < N; i++) {
+        int grauSaida = 0;
+        for (int j = 0; j < N; j++) {
+            if (grafo[i][j] == 1) {
+                grauSaida++;
+            }
+        }
+
+        if (grauSaida == 0) {
+            printf("- %s\n", paginas[i]);
+        }
+    }
+    printf("\n");
+}
+
+void verificarConectividade(int grafo[N][N]) {
+    bool visitado[N];
+    for(int i = 0; i < N; i++) visitado[i] = false;
+
+    int fila[N];
+    int frente = 0;
+    int tras = 0;
+
+    int inicio = 0;
+    fila[tras++] = inicio;
+    visitado[inicio] = true;
+    int contagemVisitados = 0;
+
+    while (frente < tras) {
+        int atual = fila[frente++];
+        contagemVisitados++;
+
+        for (int i = 0; i < N; i++) {
+            if (!visitado[i]) {
+                if (grafo[atual][i] == 1 || grafo[i][atual] == 1) {
+                    visitado[i] = true;
+                    fila[tras++] = i;
+                }
+            }
+        }
+    }
+
+    if (contagemVisitados == N) {
+        printf("Conectividade: O grafo E conexo (fracamente conexo).\n");
+    } else {
+        printf("Conectividade: O grafo NAO E conexo.\n");
+    }
+    printf("\n");
 }
 
 int main() {
-	
-    int valor1, valor2, maior, menor;
-    printf("Insira o primeiro valor: ");
-    scanf("%d", &valor1);
-    printf("Insira o segundo valor: ");
-    scanf("%d", &valor2);
-    encontraMaiorMenor(&valor1, &valor2, &maior, &menor);
-    printf("O maior valor eh: %d \n", maior);
-    printf("O menor valor eh: %d \n", menor);
-    return 0;
+    char paginas[N][20] = {
+        "Inicio",
+        "Categorias",
+        "Video",
+        "Perfil",
+        "Historico",
+        "Busca",
+        "Config"
+    };
     
+    int grafo[N][N] = {
+        {0, 1, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 0},
+        {0, 0, 0, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 1, 0, 0, 0, 0},
+        {0, 1, 1, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0}
+    };
+    
+    imprimeGrafo(grafo, paginas);
+    
+    listarAlcancaveis(grafo, paginas, 0); 
+    
+    paginasPopulares(grafo, paginas);
+    
+    paginasSemSaida(grafo, paginas);
+    
+    verificarConectividade(grafo);
+    
+    return 0;
 }
